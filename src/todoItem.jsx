800116072355cf6098854f0acee236f4f4ea0190
FennyLiang <shras1993@gmail.com>
2016-12-21 16:37:29 +0800

    add props

--- src/todoItem.jsx
+++ src/todoItem.jsx
@@ -1,12 +1,30 @@
 import React from 'react'
+import { PropTypes } from 'react'
 
 export default class ToDoItem extends React.Component {
 
+  static propTypes = {
+    title: PropTypes.string,
+    completed: PropTypes.bool
+  }
+
+  static defaultProps = {
+    ToDoItem: {
+      title: '',
+      completed: false,
+    }
+  }
+
   render() {
+    const {
+      title,
+      completed
+    } = this.props;
+
     return (
       <div>
-        <input type="checkbox" />
-        <span> Buy lunch </span>
+        <input type="checkbox" checked={completed}/>
+        <span>{title}</span>
         <button>X</button>
       </div>
 
