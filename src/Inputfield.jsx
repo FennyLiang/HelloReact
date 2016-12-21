2e0a0385989c29b6546b22b1f1a5c53cf5cb594b
FennyLiang <shras1993@gmail.com>
2016-12-21 16:36:40 +0800

    Replace to props

--- src/Inputfield.jsx
+++ src/Inputfield.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default class Inputfield extends React.Component {
 
   render() {
-    return <input type="text" placeholder="add events" style={{marginTop: 20 }} />;
+    return <input type="text" {...this.props} />;
 
   }
 }
