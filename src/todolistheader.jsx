630cc6d2923c16c5fe6ff6969ef3da44ecdc9c03
FennyLiang <shras1993@gmail.com>
2016-12-21 16:38:02 +0800

    replace to props

--- src/todolistheader.jsx
+++ src/todolistheader.jsx
@@ -3,10 +3,16 @@ import React from 'react';
 export default class TodoHeader extends React.Component {
 
   render() {
+    const {
+      title,
+      name,
+      todoCount
+    } = this.props
+
     return (
       <div>
-        <h1 id="title">我的待辦清單</h1>
-        <span className="header_content">哈囉，James：你有 99 項未完成待辦事項</span>
+        <h1>{title}</h1>
+        <span>哈囉，{name}：你有 {todoCount} 項未完成待辦事項</span>
       </div>
     );
 
